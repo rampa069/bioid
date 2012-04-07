4ae49b06a56ebc062ed775b42aeaaeb3b46844b0
root <root@bioid.tb-solutions.com>
2012-04-07 04:55:18 +0200

    workfiles erased.

--- scripts/procesausers.sh
+++ scripts/procesausers.sh
@@ -7,34 +7,35 @@ CFGDIR=$BIODIR/cfg
 NDXDIR=$BIODIR/ndx
 LSTDIR=$BIODIR/lst
 TRAINDIR=$BIODIR/train
+SCRIPTDIR=$BIODIR/scripts
 #
 #
 
 rm $BIODIR/prm/*
 rm $BIODIR/lbl/*
 
-echo "" > $LSTDIR/users.lst
+echo "" > $LSTDIR/users_train.lst
 
-for a in `find $TRAINDIR -name *-digitos.wav` ; 
+for a in `find $TRAINDIR -name *-digitos*.wav` ; 
 do  
         c=`basename $a .wav`
           
         sfbcep -F WAVE -p 19 -e -D -A $a prm/$c.prm
                     
-        echo $c >> $LSTDIR/users.lst                         
+        echo $c >> $LSTDIR/users_train.lst                         
 done
                                  
 #
 #
-$BINDIR/NormFeat --config $CFGDIR/NormFeat_energy.cfg --inputFeatureFilename $LSTDIR/users.lst  
+$BINDIR/NormFeat --config $CFGDIR/NormFeat_energy.cfg --inputFeatureFilename $LSTDIR/users_train.lst  
 #
-$BINDIR/EnergyDetector --config $CFGDIR/EnergyDetector.cfg --inputFeatureFilename $LSTDIR/users.lst 
+$BINDIR/EnergyDetector --config $CFGDIR/EnergyDetector.cfg --inputFeatureFilename $LSTDIR/users_train.lst 
 #
-$BINDIR/NormFeat --config $CFGDIR/NormFeat.cfg --inputFeatureFilename $LSTDIR/users.lst 
+$BINDIR/NormFeat --config $CFGDIR/NormFeat.cfg --inputFeatureFilename $LSTDIR/users_train.lst 
 #
 #
-$BINDIR/TrainWorld --config $CFGDIR/TrainWorldInit.cfg --inputStreamList $LSTDIR/users.lst  --outputWorldFilename world_init --debug true --verbose true
-$BINDIR/TrainWorld --config $CFGDIR/TrainWorldFinal.cfg --inputStreamList $LSTDIR/users.lst --outputWorldFilename world --inputWorldFilename world_init
+$BINDIR/TrainWorld --config $CFGDIR/TrainWorldInit.cfg --inputStreamList $LSTDIR/users.lst  --outputWorldFilename world_init --debug true --verbose true --weightStreamList $LSTDIR/users.weight
+$BINDIR/TrainWorld --config $CFGDIR/TrainWorldFinal.cfg --inputStreamList $LSTDIR/users.lst --outputWorldFilename world --inputWorldFilename world_init  --weightStreamList $LSTDIR/users.weight
 #
 # --weightStreamList $LSTDIR/world.weight
 #
@@ -51,7 +52,7 @@ $BINDIR/TrainWorld --config $CFGDIR/TrainWorldFinal.cfg --inputStreamList $LSTDI
 #
 for a in `ls $BIODIR/train/wav` ;
 do
-	$BIODIR/train2.sh $a
+	$SCRIPTDIR/train.sh $a
 	                
 done
                 
